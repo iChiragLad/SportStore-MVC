a51c0727bdb665cd976e6f8365eb6262407b6f1b
Chirag Lad <chiragladn92@gmail.com>
2017-02-15 00:59:00 +0530

    15-02-2017 - Project structure, Ninject, Moq, Dependency setup

---
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Moq;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private readonly IKernel _kernel;
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            _kernel = kernel;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product() {Name = "Football", Price = 25},
+                new Product() {Name = "Surf Board", Price = 179},
+                new Product() {Name = "Running Shoes", Price = 95}
+
+            });
+
+            _kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
+    }
+}
