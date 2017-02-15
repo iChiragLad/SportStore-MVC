768db1f19a66e1380d64a701c95cb0b94d56fb95
Chirag Lad <chiragladn92@gmail.com>
2017-02-15 09:12:22 +0530

    Added first controller and view

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _repository;
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_repository.Products);
+        }
+    }
+}
