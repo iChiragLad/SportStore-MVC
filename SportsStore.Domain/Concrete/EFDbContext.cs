c9e7c017ad505db464c14a6416ad8588a0c8e062
Chirag Lad <chiragladn92@gmail.com>
2017-02-16 09:27:40 +0530

    16-02-2017 - Integrating EF database

---
+++ SportsStore.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext() : base("EFDbContext")
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; } 
+    }
+}
--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Web.Mvc;
 using Moq;
 using Ninject;
+using Ninject.Web.Common;
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
@@ -29,16 +31,7 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns(new List<Product>
-            {
-                new Product() {Name = "Football", Price = 25},
-                new Product() {Name = "Surf Board", Price = 179},
-                new Product() {Name = "Running Shoes", Price = 95}
-
-            });
-
-            _kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            _kernel.Bind<IProductRepository>().To<EFProductRepository>().InTransientScope();
         }
     }
 }
